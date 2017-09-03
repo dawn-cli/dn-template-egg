2671ae5d7211e279e35b41999cc7823077ae16a4
行剑 <sinian.ssn@alibaba-inc.com>
2017-09-03 14:22:59 +0800

    [modify] Big Bang !

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1504417720833_5934';
+
+  // add your config here
+
+  return config;
+};
